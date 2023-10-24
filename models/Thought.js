0c0ff485105ab7f5aaafd90e425aeeeacf8135b6
Owen Lang <owen.lang146@gmail.com>
2023-10-23 20:41:35 -0500

    thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,24 @@
 const { Schema, model } = require('mongoose');
-
+const {reactionSchema} = require('./Reaction')
 // Schema to create a course model
-const courseSchema = new Schema(
+const thoughtSchema = new Schema(
   {
-    courseName: {
+    thoughtText: {
       type: String,
       required: true,
+      minLength: 1,
+      maxLength: 280
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
-    },
-    startDate: {
+    createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now()
     },
-    endDate: {
-      type: Date,
-      // Sets a default value of 12 weeks from now
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    username: {
+      type: String,
+      required: true
     },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
+    reactions: [reactionSchema]
+
   },
   {
     toJSON: {
@@ -35,6 +28,12 @@ const courseSchema = new Schema(
   }
 );
 
-const Course = model('course', courseSchema);
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
 
-module.exports = Course;
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
